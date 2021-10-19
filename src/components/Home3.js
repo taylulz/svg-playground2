8a9853b7017a2c7fa6819d39aca67ad343826da6
taylor d <taylor.delph@eyecuelab.com>
2021-10-18 17:12:45 -0700

    add third svg component

---
+++ src/components/Home3.js
@@ -0,0 +1,14 @@
+import { ReactComponent as Logo } from '../img/logo.svg';
+import { Container } from 'react-bootstrap';
+
+function Home3() {
+  return (
+    <>
+      <Container>
+        <Logo className="App-logo" alt="logo"/>
+      </Container>
+    </>
+  );
+}
+
+export default Home3;
